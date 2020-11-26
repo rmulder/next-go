a8564996072eea11e6c30745693fcb2feec167e8
Robert Matthew Mulder <rmmulder@gmail.com>
2020-11-26 14:10:15 -0800

    Fix warning

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 import 'styles/global-styles'
 
@@ -8,7 +8,7 @@ export default class SiteDocument extends Document {
     const main = sheet.collectStyles(<Main />)
     const styleTags = sheet.getStyleElement()
     return (
-      <html>
+      <Html>
         <Head>
           <meta charSet='utf-8' />
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
@@ -22,7 +22,7 @@ export default class SiteDocument extends Document {
           </div>
           <NextScript />
         </body>
-      </html>
+      </Html>
     )
   }
 }
