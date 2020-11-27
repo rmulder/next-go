ad4b9d67e7956a7d571d57d2904427e3ebc1cf81
Robert Matthew Mulder <rmmulder@gmail.com>
2020-11-26 18:20:58 -0800

    Remove viewport warning

--- layouts/Main.js
+++ layouts/Main.js
@@ -6,6 +6,7 @@ import Footer from 'components/Footer'
 const Main = ({ children, title = 'This is the default title' }) => <Wrapper>
   <Head>
     <title>{ title }</title>
+    <meta name='viewport' content='initial-scale=1.0, width=device-width' />
   </Head>
   <header>
     <Nav />
